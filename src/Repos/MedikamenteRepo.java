5c77fb3ee3bb00d7ef5a2b94157b2e3fbdca7620
magureanandrei <andimagurean714@gmail.com>
2025-01-15 15:18:11 +0200

    created repos

--- src/Repos/MedikamenteRepo.java
+++ src/Repos/MedikamenteRepo.java
@@ -1,4 +1,31 @@
 package Repos;
 
+import Modelle.Medikamente;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MedikamenteRepo {
+    Map<String, Medikamente> medikamente= new HashMap<>();
+
+    public void add(Medikamente obj) {
+        medikamente.put(obj.getName(), obj);
+    }
+
+    public void remove(Integer id) {
+        medikamente.remove(id);
+    }
+
+    public void update(Medikamente obj) {
+        medikamente.replace(obj.getName(), obj);
+    }
+
+    public Medikamente get(Integer getId) {
+        return medikamente.get(getId);
+    }
+
+    public List<Medikamente> getAll(){
+        return medikamente.values().stream().toList();
+    }
 }
--- src/Repos/PatientenRepo.java
+++ src/Repos/PatientenRepo.java
@@ -1,4 +1,32 @@
 package Repos;
 
+import Modelle.Patienten;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PatientenRepo {
+    Map<Integer, Patienten> patienten=new HashMap<>();
+
+    public void add(Patienten obj) {
+        patienten.put(obj.getId(), obj);
+    }
+
+    public void remove(Integer id) {
+        patienten.remove(id);
+    }
+
+    public void update(Patienten obj) {
+        patienten.replace(obj.getId(), obj);
+    }
+
+    public Patienten get(Integer getId) {
+        return patienten.get(getId);
+    }
+
+    public List<Patienten> getAll(){
+        return patienten.values().stream().toList();
+    }
+
 }
